b698d9f658736e146235b56e748f11db0ec842a1
Luc Baumeler <lucbaumeler@MacBook-Pro-von-Luc.local>
2025-05-03 21:03:21 +0200

    nice

--- wezterm.lua
+++ wezterm.lua
@@ -3,10 +3,12 @@ local wezterm = require("wezterm")
 return {
 	font = wezterm.font("JetBrains Mono"),
 	font_size = 13.0,
-	color_scheme = "Dracula",
+	color_scheme = "Tokyo Night Moon",
 	enable_tab_bar = false,
 	use_fancy_tab_bar = false,
-	window_background_opacity = 0.95,
+	window_background_opacity = 0.6,
+	window_decorations = "NONE",
+	default_cursor_style = "BlinkingBar",
 	keys = {
 		{ key = "t", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("DefaultDomain") },
 		{ key = "w", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
